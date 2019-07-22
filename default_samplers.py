5e2d1aabbc52f7493d6c42367da6e471bec730ca
Antonio Herrera Martin <ahe106@canterbury.ac.nz>
2019-07-22 14:38:21 +1200

    Added Emcee sampling with nested-presampling.

--- default_samplers.py
+++ default_samplers.py
@@ -4,6 +4,8 @@ import dynesty
 from dynesty import plotting as dyplot
 import likelihoodfunctions_galmod as likemod
 from galmoddefinitions import probte
+from galmoddefinitions import thetae_func
+import utils
 
 class NestedSampling():
     """ This implementes a defaul nested sampling for
@@ -36,3 +38,120 @@ class NestedSampling():
         fig.savefig('./evidence.png')
         fig1.savefig('./tracers.png')
         fig2.savefig('./cornerplot.png')
+################################################################
+class NestedEmcee:
+    """Class with a short Nested sampling followed by an Emcee run."""
+    def __init__(self, filesamples, nbins=20, column=6, prefix='Emcee', defaul_labels=None):
+        self.xval, self.val = probte(filesamples, column, nbins)
+        self.res = None
+        self.plotprefix = prefix
+        if defaul_labels is None:
+            self.labels = ['ogm', 'x', 'Ds', 'Vs']
+        else:
+            self.labels = defaul_labels
+        self._nwalkers = 100
+        self._nsteps = 100
+        self._max_burnin_iterations = 20
+        self._nsteps_production = 2000
+
+    def _log_likelihood(self, sample):
+        """ Simple wrapper for the log likelihood """
+
+        output = likemod.logprob_sourcenorm(sample, self.xval, self.val)
+        return output
+
+    def logprob(self, variables):
+        """ Simple Wrapper for the log probability"""
+        if (-2 <= variables[0] <= 1.8) and  (0 < variables[1] <= 1) \
+            and (0 < variables[2] <= 11000) and (0 < variables[3] <= 10):
+            thetae = thetae_func(variables)
+            if 8000 > thetae >= 72.27:
+                output_log = likemod.logprob_sourcenorm(variables, self.xval, self.val)
+                return output_log
+            else:
+                return -np.inf
+        else:
+            return -np.inf
+
+
+    def _emcee_fit(self, parameters):
+        import scipy.optimize as opt
+        import emcee
+
+        nwalkers = self._nwalkers
+        nsteps = self._nsteps
+        max_burnin_iterations = self._max_burnin_iterations
+        nsteps_production = self._nsteps_production
+
+        #print('Initial parameters:', parameters)
+        print('ln Prob = ', self.logprob(parameters))
+
+        ndim = len(parameters)
+
+        print('Optimising...')
+
+        neglogprob = lambda parameters: -self.logprob(parameters)
+        res = opt.minimize(neglogprob, parameters, method='Nelder-Mead')
+
+        parameters = res.x
+        print('Optimized parameters:', parameters)
+        print('ln Prob = ', self.logprob(parameters))
+
+        #print('ndim, ',ndim, len(parameters), nwalkers)
+        state = [parameters + 1e-8 * np.random.randn(ndim) for i in range(nwalkers)]
+
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, self.logprob)
+
+        print("Running burn-in...")
+
+        iteration = 0
+
+        print('ndim, walkers, nsteps, max_iterations:', \
+            ndim, nwalkers, nsteps, max_burnin_iterations)
+
+        while iteration < max_burnin_iterations:
+
+            state, _, _ = sampler.run_mcmc(state, nsteps)
+
+            iteration += 1
+            print('iteration', iteration, 'completed')
+
+            kmax = np.argmax(sampler.flatlnprobability)
+            parameters = sampler.flatchain[kmax, :]
+
+            np.save(self.plotprefix+'-chain-burnin', sampler.flatchain)
+            np.save(self.plotprefix+'-lnp-burnin', sampler.flatlnprobability)
+            utils.plot_chain(sampler, ndim, self.labels, self.plotprefix, suffix='-burnin.png')
+
+        print("Running production...")
+
+        sampler.reset()
+
+        state, _, _ = sampler.run_mcmc(state, nsteps_production)
+        utils.plot_chain(sampler, ndim, self.labels, self.plotprefix, suffix='-production.png')
+
+        np.save(self.plotprefix+'-chain-production', sampler.flatchain)
+        np.save(self.plotprefix+'-lnp-production', sampler.flatlnprobability)
+        np.save(self.plotprefix+'-state-production', np.asarray(state))
+        np.save(self.plotprefix+'-min_chi2-production', \
+            np.asarray(sampler.flatchain[np.argmax(sampler.flatlnprobability)]))
+
+        print('Finished')
+
+    def run(self, dlogz=10):
+        """Main method to combine short nested sampling and Emcee"""
+
+        ndim = len(self.labels)
+
+        print('Initial Nested sampling')
+        sampler = dynesty.NestedSampler(self._log_likelihood, likemod.pt_te, ndim, nlive=100)
+        sampler.run_nested(dlogz=dlogz)
+        res = sampler.results
+        print("\nStarting Emcee")
+        initial_value = res['samples'][-1]
+        print("Initial values are:")
+        print("{:s} = {:f}".format(self.labels[0], initial_value[0]))
+        print("{:s} = {:f}".format(self.labels[1], initial_value[1]))
+        print("{:s} = {:f}".format(self.labels[2], initial_value[2]))
+        print("{:s} = {:f}".format(self.labels[3], initial_value[3]))
+        self._emcee_fit(initial_value)
